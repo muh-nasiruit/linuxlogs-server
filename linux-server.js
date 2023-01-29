00ce3624720ca11b20452793e7c73cb43c3a226e
Nasir Mahmood <n_mahmood999@yahoo.com>
2023-01-29 18:49:48 +0000

    API for linux system logs

--- linux-server.js
+++ linux-server.js
@@ -3,14 +3,17 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
-const port = process.env.PORT || 4000;
+const spawn = require('child_process').spawn;
+const port = process.env.PORT || 4054;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.post('/api/linux-logs', (req, res) => {
-    res.send('LINUX MACHINE');
+    	const tail = spawn('tail', ['-n 5', '/var/log/syslog']);
+	console.log("Sending log");
+	tail.stdout.pipe(res);
+
 })
 
 app.listen(port, () => {
